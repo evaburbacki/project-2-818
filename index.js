276ec153f135c3b839ccf11f8d649f7a72cba6de
evab1209 <evaburbacki@gmail.com>
2020-11-18 19:23:04 -0500

    ready for deployment

--- index.js
+++ index.js
@@ -1,56 +1,57 @@
-const express = require('express')
-const app = express()
-const ejsLayouts = require('express-ejs-layouts')
-const session = require('express-session')
-const passport = require('./config/ppConfig')
-const flash = require('connect-flash')
-const isLoggedIn = require('./middleware/isLoggedIn')
-
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const ejsLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const passport = require("./config/ppConfig");
+const flash = require("connect-flash");
+const isLoggedIn = require("./middleware/isLoggedIn");
 
 // views (ejs and layouts) set up
-app.set('view engine', 'ejs')
-app.use(ejsLayouts)
+app.set("view engine", "ejs");
+app.use(ejsLayouts);
 
 // body parser middelware
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
 // session middleware
-app.use(session({
-    secret: 'keyboard cat',
+app.use(
+  session({
+    secret: "process.env.SESSION_SECRET",
     resave: false,
-    saveUninitialized: true
-}))
+    saveUninitialized: true,
+  })
+);
 
 // passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // flash middleware (must go AFTER session middleware)
-app.use(flash())
+app.use(flash());
 
 // custom middleware
 app.use((req, res, next) => {
-    // before every route, attach the flash messages and current user to res.locals
-    res.locals.alerts = req.flash();
-    res.locals.currentUser = req.user;
-    next()
-})
+  // before every route, attach the flash messages and current user to res.locals
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
 
-// controllers middleware 
-app.use('/auth', require('./controllers/auth'))
-
+// controllers middleware
+app.use("/auth", require("./controllers/auth"));
 
 // home route
-app.get('/', (req, res)=>{
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 // profile route
-app.get('/profile', isLoggedIn, (req, res)=>{
-    res.render('profile')
-})
+app.get("/profile", isLoggedIn, (req, res) => {
+  res.render("profile");
+});
 
-
-app.listen(3000, ()=>{
-    console.log("auth_practice running on port 3000")
-})
+app.listen(process.env.PORT, () => {
+  console.log("you are listening to the sounds of 8000");
+});
